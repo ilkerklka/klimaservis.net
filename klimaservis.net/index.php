88d747000018c2b486788213de589dbf1ccddce9
İlker Kücükyılmaz <ikoculuk@gmail.com>
2022-05-25 13:58:26 +0300

    footer ve navbar yapıldı

---
+++ klimaservis.net/index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klima Servis Merkezi - Profesyonel ve Özel Klima Teknik Servis</title>
+    <link rel="stylesheet" href="style.css">
+
+
+
+    <!--BOOSTRAP-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--BOOSTRAP-->
+
+
+
+    <!--MDB-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css" rel="stylesheet"/>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"></script>
+    <!--MDB-->
+
+
+
+    <!--FONTAWSOME-->
+    <script src="https://kit.fontawesome.com/371afa92ba.js" crossorigin="anonymous"></script>
+    <!--FONTAWSOME-->
+
+
+
+    <!--ICON FINDER-->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!--ICON FINDER-->
+
+
+
+    <!--ANIMATE.CSS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!--ANIMATE.CSS-->
+</head>
+<body>
+  <!-- HEAD -->
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+              <a  class="navbar-brand" href="#">
+                <img src="logo.png" alt="" width="220" height="100" class="d-inline-block align-text-top">
+                
+              </a>
+              <a class="online" style="border: 0px; border-radius: 60px; padding: 10px 40px; background-color: #373795; color: white;" 
+              href="">Online Randevu Al</a>
+            </div>
+          </nav>
+    </header>
+    <!-- HEAD -->
+
+
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark  "  style=" background-color:#373795 ;"> 
+  <div  class="  container-fluid">
+    
+    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon "></span>
+    </button>
+    <div class="collapse navbar-collapse  justify-content-center" id="navbarNavDropdown">
+      <ul class="navbar-nav ">
+      <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Kurumsal
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="Hakkimizda">Hakkımızda</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Bilgiler
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Klima Servis Blog</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Hizmetlerimiz
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Klima Bakım Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Klima Tamir Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Klima Kumandası</a></li>
+            <li><a class="dropdown-item" href="#">Klima Gaz Dolumu</a></li>
+            <li><a class="dropdown-item" href="#">Klima Montaj Servisi</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Klima Servisi
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Arçelik Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Beko Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Bosh Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Samsung Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Baymak Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Altus Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">LG Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">LG Beko Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Vestel Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">York Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Sigma Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">ECA Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Midea Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Airfel Klima Servisi</a></li>
+            <li><a class="dropdown-item" href="#">Akai Klima Servisi</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">iletişim Bilgileri</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <!-- NAVBAR -->
+
+
+  
+
+    <!-- FOOTER -->
+    <!-- Footer -->
+<footer class="text-center text-lg-start text-muted" style="background-color: #1c1c1c;">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Sosyal medya hesaplarımız için :</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <a  class="navbar-brand" href="#">
+                <img src="logo.png" alt="" width="220" height="100" class="d-inline-block align-text-top">
+                
+              </a> <br>
+              <hr>
+          <p>
+          Marka ve modeli ne olursa olsun, tüm klimaların klima bakımı, 
+          klima montajı, klima sökme-takma, klima arızası, klima yer değişimi, 
+          sıfır ve 2. el klima satışı, teknik destek, tamir, bakım ve onarım hizmeti vermekteyiz.
+          <br>  <br> Klima servisinde uzman ve profesyonel teknik servis firmalarıyla çalışmanız cihazlarınızda daha büyük sorunlar oluşmaması için gereklidir.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Bilgilerndirme
+            <hr>
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">- Bize Ulaşın</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Klima Servis Blog</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Online Randevu Al</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- En Yakın Servisi Bul</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Hizmetlerimiz
+            <hr>
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">- Klima Bakım Servis</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Klima Tamir Servis</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Klima Montaj Servis</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Klima Kumandası</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Klima Gaz Dolumu</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Klima Servisi
+            <hr>
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">- Arçelik Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Altus Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Baymak Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Beko Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Bosch Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- LG  Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- LG Beko Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Samsung Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- Vestel Klima Servisi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">- York Klima Servisi</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Ulaşım Bilgileri
+            <hr>
+          </h6>
+          <p><i class="fas fa-home me-3"></i><b>Boğaçiçi Klima Servisi</b><br> İstanbul Anadolu Yakası Geneli Aynı Gün Klima Servis Hizmeti Verilmektedir.</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            destek@klimaservis.net
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +90 530 666 4347</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Tüm Hakları Saklıdır :
+    <a class="text-reset fw-bold" href="https://klimaservis.net/">klimaservis.net</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+    <!-- FOOTER -->
+</body>
+</html>
